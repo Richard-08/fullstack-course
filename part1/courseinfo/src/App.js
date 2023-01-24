de8e2039a17ac587a255b1194f067c339ebe0d54
Bulat0895 <bulatahtyamov1995@gmail.com>
2023-01-24 12:40:31 +0300

    feat(part1): courseinfo completed

---
+++ part1/courseinfo/src/App.js
@@ -0,0 +1,54 @@
+const Header = (props) => {
+  return <h1>{props.course}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.name} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      {props.parts.map((part) => (
+        <Part key={part.name} name={part.name} exercises={props.exercises} />
+      ))}
+    </div>
+  );
+};
+
+const Total = (props) => {
+  const total = props.parts.reduce((total, val) => total + val.exercises, 0);
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
+    </div>
+  );
+};
+
+export default App;
