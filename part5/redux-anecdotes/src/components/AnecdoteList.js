b9f3d8c45686e5cbdc131129b208af1f7579dc76
Bulat0895 <bulatahtyamov1995@gmail.com>
2023-08-01 22:29:04 +0300

    fix

---
+++ part5/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,41 @@
+import { useSelector, useDispatch } from "react-redux";
+import { voteById } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const anecdotes = useSelector((state) => {
+    const search = state.filter.toLowerCase();
+
+    if (search) {
+      return state.anecdotes.filter((item) =>
+        item.content.toLowerCase().includes(search)
+      );
+    }
+
+    return state.anecdotes;
+  });
+  const dispatch = useDispatch();
+
+  const vote = (id) => {
+    dispatch(voteById(id));
+  };
+
+  const sorted_anecdotes = () => {
+    return anecdotes.sort((a, b) => b.votes - a.votes);
+  };
+
+  return (
+    <div>
+      {sorted_anecdotes().map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default AnecdoteList;
