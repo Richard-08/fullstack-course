9fcb7437542be68d5f1d8de443fbbaa78d7fe8b1
Bulat0895 <bulatahtyamov1995@gmail.com>
2023-09-15 08:26:02 +0300

    feat(part7): 7.4-7.6 execises completed

--- part7/routed-anecdotes/src/App.jsx
+++ part7/routed-anecdotes/src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 
 import { Routes, Route, Link, useMatch, useNavigate } from "react-router-dom";
 
+import { useField } from "./hooks/useField";
+
 const Menu = () => {
   const padding = {
     paddingRight: 5,
@@ -87,18 +89,24 @@ const Footer = () => (
 );
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
+  };
+
+  const resetField = () => {
+    content.reset();
+    author.reset();
+    info.reset();
   };
 
   return (
@@ -109,27 +117,36 @@ const CreateNew = (props) => {
           content
           <input
             name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
         <div>
           author
           <input
             name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
         <div>
           url for more info
           <input
             name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
+            type={info.type}
+            value={info.value}
+            onChange={info.onChange}
           />
         </div>
         <button>create</button>
+        <button
+          type="button"
+          onClick={resetField}
+        >
+          reset
+        </button>
       </form>
     </div>
   );
